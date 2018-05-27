8843f8db41cf2b3dde2ece3e3dc3823db1fd9021
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-05-27 12:09:23 -0400

    ifdef removed

--- lsrec/srcs/main.c
+++ lsrec/srcs/main.c
@@ -15,54 +15,57 @@ list_dir (const char * dir_name)
                  dir_name, strerror (errno));
         exit (EXIT_FAILURE);
     }
-    while (1) {
+    while (1) 
+    {
         struct dirent * entry;
         const char * d_name;
 
         /* "Readdir" gets subsequent entries from "d". */
         entry = readdir (d);
-        if (! entry) {
+        if (! entry)
+        {
             /* There are no more entries in this directory, so break
                out of the while loop. */
             break;
         }
         d_name = entry->d_name;
         /* Print the name of the file and directory. */
-    if (d_name[0] != '.')
-	   printf("%s\n", d_name);
+        if (d_name[0] != '.')
+	       printf("%s\n", d_name);
 
-#if 0
+// #if 0
 	/* If you don't want to print the directories, use the
 	   following line: */
 
-        if (! (entry->d_type & DT_DIR)) {
-	    printf ("%s/%s\n", dir_name, d_name);
-	}
+ //        if (! (entry->d_type & DT_DIR)) {
+	//     printf ("%s/%s\n", dir_name, d_name);
+	// }
 
-#endif
-
-
-        if (entry->d_type & DT_DIR) {
-
-            /* Check that the directory is not "d" or d's parent. */
+// #endif
 
 
-            //STRJOIN INTO BUFFER AND PRINT BUFFER AT THE END
-            
-            if (ft_strcmp (d_name, "..") != 0 && ft_strcmp (d_name, ".") != 0) {
+        if (entry->d_type & DT_DIR) 
+        {
+
+                /* Check that the directory is not "d" or d's parent. */
+// STRJOIN INTO BUFFER AND PRINT BUFFER AT THE END
+                
+            if (ft_strcmp (d_name, "..") != 0 && ft_strcmp (d_name, ".") != 0) 
+            {
                 int path_length = 0;
                 char path[PATH_MAX];
- 
+     
                 path_length = snprintf (path, PATH_MAX, "%s/%s", dir_name, d_name);
                 printf ("snprintf: %s\n", path);
-                if (path_length >= PATH_MAX) {
+                if (path_length >= PATH_MAX) 
+                {
                     ft_printf ("Path length too long.\n");
                     exit (EXIT_FAILURE);
                 }
-                /* Recursively call "list_dir" with the new path. */
+                    /* Recursively call "list_dir" with the new path. */
                 list_dir (path);
             }
-	   }
+    	}
     }
     /* After going through all the entries, close the directory. */
     if (closedir (d)) {
