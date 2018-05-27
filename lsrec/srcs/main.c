feaf13b96d2064fda42dc9cbb6525b344c85c641
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-05-27 15:35:06 -0400

    strcat instead of strjoin avoid malloc

--- lsrec/srcs/main.c
+++ lsrec/srcs/main.c
@@ -1,28 +1,27 @@
 #include "../ft_ls.h"
 
 static void
-list_dir (const char * dir_name)
+list_dir (const char *dir_name)
 {
-    DIR * d;
+    DIR *d;
 
     /* Open the directory specified by "dir_name". */
-
     d = opendir (dir_name);
 
     /* Check it was opened. */
-    if (! d) {
+    if (!d) {
         fprintf (stderr, "mehCannot open directory '%s': %s\n",
                  dir_name, strerror (errno));
         exit (EXIT_FAILURE);
     }
     while (1) 
     {
-        struct dirent * entry;
-        const char * d_name;
+        struct dirent *entry;
+        const char *d_name;
 
         /* "Readdir" gets subsequent entries from "d". */
-        entry = readdir (d);
-        if (! entry)
+        entry = readdir(d);
+        if (!entry)
         {
             /* There are no more entries in this directory, so break
                out of the while loop. */
@@ -31,31 +30,21 @@ list_dir (const char * dir_name)
         d_name = entry->d_name;
         /* Print the name of the file and directory. */
         if (d_name[0] != '.')
-	       printf("%s\n", d_name);
-
-// #if 0
-	/* If you don't want to print the directories, use the
-	   following line: */
-
- //        if (! (entry->d_type & DT_DIR)) {
-	//     printf ("%s/%s\n", dir_name, d_name);
-	// }
-
-// #endif
-
-
+	       printf("d_name: %s\n", d_name);
         if (entry->d_type & DT_DIR) 
         {
-
-                /* Check that the directory is not "d" or d's parent. */
+            /* Check that the directory is not "d" or d's parent. */
 // STRJOIN INTO BUFFER AND PRINT BUFFER AT THE END
-                
+    //-strcat will append instead of allocate
             if (ft_strcmp (d_name, "..") != 0 && ft_strcmp (d_name, ".") != 0) 
             {
+                // char newpath[PATH_MAX];
                 int path_length = 0;
                 char path[PATH_MAX];
-     
+                printf("(dir_name: %s\t\td_name: %s)\n", dir_name, d_name);
                 path_length = snprintf (path, PATH_MAX, "%s/%s", dir_name, d_name);
+//DOES SNPRINTF AUTOMATICALLY FREE THE BUFFER?
+    //just allocate on the stack for char[PATH_NAME] to avoid allocating memory
                 printf ("snprintf: %s\n", path);
                 if (path_length >= PATH_MAX) 
                 {
