f8a7ad1a50e58953da073a005d45168182a3f659
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-05-29 08:25:16 -0400

    ls and ls a done

--- ft_ls1/srcs/list_dir.c
+++ ft_ls1/srcs/list_dir.c
@@ -1,28 +1,5 @@
 #include "../ft_ls.h"
 
-char	*extract_path(char *str)
-{
-	int				len;
-	char			*newstr;
-
-	newstr = (char *)malloc(sizeof(char));
-	len = ft_strlen(str) - ft_strlen(ft_strstr(str, "/"));
-	ft_strncpy(newstr, str, len);
-	// printf("newstr: %s\n", newstr);
-	return (newstr);
-}
-
-// SEE IF THERE'S A / IN THE ARGV[1]
-// SPLIT THE STRING
-// CHECK IF EACH DIRECTORY IS VALID IN A WHILE LOOP
-
-// SEE IF THERE'S A / IN ARGV[1]
-// SPLIT THE STRING
-// SEE IF YOU CAN IT. IF IT'S A FILE, PRINT ARGV[1]
-
-
-
-
 void	list_dir(int argc, char **argv)
 {
 	DIR				*dip;
@@ -36,30 +13,13 @@ void	list_dir(int argc, char **argv)
 	{
 		if (j > 1 && j < argc)
 			write(1, "\n", 1);
-		//check for whether it's a directory or file here
-		// you're going to need another function to do that
 		dip = opendir(argv[j]);
-		// if(stat(argv[j],&fileStat) < 0)  
-  //       	ft_error("stat error");
-  //       else
-  //       	ft_error("cool");
 		if (dip == NULL)
 		{
-			// extract_path(argv[j]);
-
-			while ((dip = opendir(extract_path(argv[j]))) != NULL)
-			{
-				if(stat(argv[j],&fileStat) < 0)  
+				if(stat(argv[j],&fileStat) < 0) 
         			ft_error("stat error");
-				// if (!dip)
-				// {
-				// 	ft_error("annot open dir\n");
-				// }
 				printf("%s\n", argv[j]);
-				exit(0);
-			}
-			// else
-				ft_error("opendir");
+				exit(1);
 		}
 		while ((dit = readdir(dip)) != NULL)
 		{
@@ -77,6 +37,7 @@ void	list_dira(int argc, char **argv)
 {
 	DIR				*dip;
 	struct dirent	*dit;
+	struct stat 	fileStat;
 	int 			i;
 	int				j;
 	i = 0;
@@ -85,11 +46,14 @@ void	list_dira(int argc, char **argv)
 	{
 		if (j > 2 && j < argc)
 			write(1, "\n", 1);
-		//check for whether it's a directory or file here
-		// you're going to need another function to do that
 		dip = opendir(argv[j]);
 		if (dip == NULL)
-			ft_error("opendir");
+		{
+				if(stat(argv[j],&fileStat) < 0)  
+        			ft_error("stat error");
+				printf("%s\n", argv[j]);
+				exit(1);
+		}
 		while ((dit = readdir(dip)) != NULL)
 		{
 			ft_printf("%s\n", dit->d_name);
