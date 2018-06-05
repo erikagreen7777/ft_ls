ddaf9afe74b576967eb73a4680a12f382cdd7d52
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-06-05 11:28:29 -0400

    normed ft_strcmp. TODO: ls errors

--- ft_ls1/libft/ft_strcmp.c
+++ ft_ls1/libft/ft_strcmp.c
@@ -12,23 +12,13 @@
 
 #include "libft.h"
 
-// int		ft_strcmp(const char *s1, const char *s2)
-// {
-// 	size_t		i;
-
-// 	i = 0;
-// 	if (ft_strlen(s1) - ft_strlen(s2) != 0)
-// 		return (ft_strlen(s1) - ft_strlen(s2));
-// 	else
-// 	{
-// 		while ((s1[i] || s2[i]) && (s1[i] == s2[i]))
-// 			i++;
-// 		return (s1[i] - s2[i]);
-// 	}
-// }
-
-int 	findthelength(int len1, int len2)
+int 	findthelength(const char *s1, const char *s2)
 {
+	int len1;
+	int len2;
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+
 	if (len1 > len2)
 		return (1);
 	else if (len2 > len1)
@@ -38,36 +28,36 @@ int 	findthelength(int len1, int len2)
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	int len1;
-	int len2;
+	// int len1;
+	// int len2;
 	int longer;
 	int i;
 	int diff;
 
 	diff = 9000;
-	i = 0;
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	longer = findthelength(len1, len2);
+	i = -1;
+	// len1 = ft_strlen(s1);
+	// len2 = ft_strlen(s2);
+	longer = findthelength(s1, s2);
 
 	if (longer == 1 || longer == 0)
 	{
-		while (s1[i])
+		while (s1[++i])
 		{
 			diff = s1[i] - s2[i];
 			if (diff != 0)
 				return (diff);
-			i++;
+			// i++;
 		}
 	}
 	else if (longer == 2)
 	{
-		while (s2[i])
+		while (s2[++i])
 		{
 			diff = s1[i] - s2[i];
 			if (diff != 0)
 				return (diff);
-			i++;
+			// i++;
 		}
 	}
 	return (0);
