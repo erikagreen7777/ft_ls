f9d3bdd0da412de0df05663d19c7b819848d319f
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-06-11 10:12:23 -0400

    recursion.c currenly operating like ls. recuse is ls -R

--- ft_ls1/srcs/recuse.c
+++ ft_ls1/srcs/recuse.c
@@ -1,5 +1,3 @@
-#include "../ft_ls.h"
-
 void list_dirbigr(char **argv, const char *dir_name, t_lists *lists)
 {
     // char    *newpath;
@@ -21,6 +19,7 @@ void list_dirbigr(char **argv, const char *dir_name, t_lists *lists)
         struct dirent *entry;
         const char *d_name;
         entry = readdir(d);
+
         if (!entry)
         {
             break;
@@ -28,33 +27,42 @@ void list_dirbigr(char **argv, const char *dir_name, t_lists *lists)
         d_name = entry->d_name;
         if (d_name[0] != '.')
 	       ft_printf("%s\n", d_name);
-        else if (entry->d_type & DT_DIR) 
+        if (entry->d_type & DT_DIR) 
         {
             /* Check that the directory is not "d" or d's parent. */
             if (ft_strcmp(d_name, "..") != 0 && ft_strcmp(d_name, ".") != 0) 
             {
+                // dir_name_length = ft_strlen(dir_name);
+                // d_name_length = ft_strlen(d_name);
+                // newpath = (char *)malloc(sizeof(dir_name_length + d_name_length + 2));
+                // ft_strcpy(newpath, dir_name);
+                // ft_strcat(newpath, "/");
+                // ft_strcat(newpath, d_name);
+                // ft_printf("%s\n", newpath);
+                // dir_name_length = 0;
+                // d_name_length = 0;
+                // ft_bzero(newpath, ft_strlen(newpath));
+                // list_dirbigr(newpath);
+
                 char newpath[PATH_MAX];
                 int  newpath_length;
                 newpath_length = ft_strlen(newpath);
                 ft_strcpy(newpath, dir_name);
-                // if (ft_strcmp(argv[2], "/") != 0){
-                ft_strcat(newpath, "/");
-//                }
+                if (ft_strcmp(argv[2], "/") != 0){
+                    ft_strcat(newpath, "/");
+                }
                 ft_strcat(newpath, d_name);
-                ft_printf("\n%s:\n", newpath);
+                ft_printf("%s\n", newpath);
                 if (newpath_length >= PATH_MAX)
                     ft_error("Path length too long");
-                lists->i++;
                     /* Recursively call "list_dir" with the new path. */
                 list_dirbigr(argv, newpath, lists);
             }
     	}
     }
     /* After going through all the entries, close the directory. */
-    if (closedir(d))
-    {
-        ft_error("closedir");
-        //exit (EXIT_FAILURE);
+    if (closedir (d)) {
+            ft_error("closedir");
+        exit (EXIT_FAILURE);
     }
 }
-
