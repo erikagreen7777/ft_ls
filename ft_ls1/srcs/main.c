b55d533e7dfe7b689f9673ef1c02d989194dc5c7
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-06-17 15:53:14 -0400

    I know parsing is a mess

--- ft_ls1/srcs/main.c
+++ ft_ls1/srcs/main.c
@@ -24,6 +24,9 @@ int main(int argc, char **argv)
 	{
 		t_lists *lists;
 		lists = NULL;
+		/*
+		** ls with no args
+		*/
 		if (argc == 1 || (argc == 2 && ft_strcmp(argv[1], ".") == 0))
 		{
 			list_dir(argc, argv);
@@ -31,13 +34,18 @@ int main(int argc, char **argv)
 		}
 		if (argc >= 2)
 		{
-
+			/*
+			** -la
+			*/
 			if ((ft_strcmp(argv[1], "-la") == 0) || (ft_strcmp(argv[1], "-al") == 0))
 			{
 				init_struct(&lists);
 				ls_la(argc, argv, lists);
 				return (0);
 			}
+			/*
+			** -lrat
+			*/
 			else if ((ft_strcmp(argv[1], "-lrat") == 0) || ft_strcmp(argv[1], "-lrta") == 0 || \
 				(ft_strcmp(argv[1], "-lart") == 0) || ft_strcmp(argv[1], "-latr") == 0 || \
 				ft_strcmp(argv[1], "-ltra") == 0 || ft_strcmp(argv[1], "-ltar") == 0 || \
@@ -55,19 +63,27 @@ int main(int argc, char **argv)
 				ls_lrat(argc, argv, lists);
 				return (0);
 			}
-			
+			/*
+			** -tl
+			*/
 			else if ((ft_strcmp(argv[1], "-tl") == 0) || ft_strcmp(argv[1], "-lt") == 0)
 			{
 				init_struct(&lists);
 				ls_lt(argc, argv, lists);
 				return (0);
 			}
+			/*
+			** -ra
+			*/
 			else if ((ft_strcmp(argv[1], "-ra") == 0) || ft_strcmp(argv[1], "-ar") == 0)
 			{
 				init_struct(&lists);
 				ls_ra(argc, argv, lists);
 				return (0);
 			}
+			/*
+			** -Ra
+			*/
 			else if ((ft_strcmp(argv[1], "-Ra") == 0) || ft_strcmp(argv[1], "-aR") == 0)
 			{
 				if (argc == 2)
@@ -81,20 +97,29 @@ int main(int argc, char **argv)
 				list_dirbigr(argv[2], 1);
 				return (0);
 			}
+			/*
+			** rat
+			*/
 			else if ((ft_strcmp(argv[1], "-rat") == 0) || ft_strcmp(argv[1], "-rta") == 0 || \
 				(ft_strcmp(argv[1], "-art") == 0) || ft_strcmp(argv[1], "-atr") == 0 || \
 				ft_strcmp(argv[1], "-tra") == 0 || ft_strcmp(argv[1], "-tar") == 0)
-			{
+			{	
 				init_struct(&lists);
 				ls_rat(argc, argv, lists);
 				return (0);
 			}
+			/*
+			** -rt
+			*/
 			else if ((ft_strcmp(argv[1], "-rt") == 0) || ft_strcmp(argv[1], "-tr") == 0)
 			{
 				init_struct(&lists);
 				ls_rt(argc, argv, lists);
 				return (0);
 			}
+			/*
+			** -tal
+			*/
 			else if ((ft_strcmp(argv[1], "-tal") == 0) || ft_strcmp(argv[1], "-lta") == 0 || \
 				(ft_strcmp(argv[1], "-lat") == 0) || ft_strcmp(argv[1], "-tla") == 0 || \
 				ft_strcmp(argv[1], "-alt") == 0 || ft_strcmp(argv[1], "-atl") == 0)
@@ -103,20 +128,30 @@ int main(int argc, char **argv)
 				ls_lta(argc, argv, lists);
 				return (0);
 			}
+			/*
+			** -a
+			*/
 			else if (ft_strcmp(argv[1], "-a") == 0)
-			{				
 				list_dira(argc, argv);
-			}
+			/*
+			** -l
+			*/
 			else if (ft_strcmp(argv[1], "-l") == 0)
 			{
 				init_struct(&lists);
 				list_dirl(argc, argv, lists);
 			}
+			/*
+			** -r
+			*/
 			else if (ft_strcmp(argv[1], "-r") == 0)
 			{
 				init_struct(&lists);
 				list_dirr(argc, argv, lists);
 			}
+			/*
+			** -R
+			*/
 			else if (ft_strcmp(argv[1], "-R") == 0)
 			{
 				if (argc == 2)
@@ -126,19 +161,24 @@ int main(int argc, char **argv)
 				}
 				init_struct(&lists);
 				lists->i = 0;
-				lists->flag = 0;
+				// lists->flag = 0;
 				R_first(argc, argv, 0);
 				list_dirbigr(argv[2], 0);
 			}
+			/*
+			** -t
+			*/
 			else if (ft_strcmp(argv[1], "-t") == 0)
 			{
 				init_struct(&lists);
 				list_dirt(argc, argv, lists);
 			}
+			/* 
+			** ls
+			*/
 			else
 				list_dir(argc, argv);
 		}
-		
 		else
 			ft_error("main: ./ft_ls error");
 	}
