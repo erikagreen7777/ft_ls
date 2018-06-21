6a227560bc1e1c210d7aa40113506323062a57e1
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-06-21 19:18:40 -0400

    Rrt added lexsort. It won't work for non r stuff. add flag

--- ft_ls1/srcs/switch.c
+++ ft_ls1/srcs/switch.c
@@ -69,8 +69,44 @@ void	ft_switch_time(t_lists *lists)
 					free(lists->temp);
 					free(lists->tempdest);
 				}
+				else if (ft_atoi(lists->timearray[lists->i]) == ft_atoi(lists->timearray[lists->j]))
+				{
+					if (ft_strcmp(lists->dest[lists->i], lists->dest[lists->j]) > 0) /* add a flag here for ls -r stuff? */
+					{
+						get_lengths(lists);
+						/*
+						** put what we're going to switch into a temporary container
+						*/
+						lists->temp = ft_strdup(lists->timearray[lists->i]);
+						lists->tempdest = ft_strdup(lists->dest[lists->i]);
+						/*
+						** clear out the first "i"
+						*/
+						ft_bzero(lists->timearray[lists->i], lists->timeleni);
+						ft_bzero(lists->dest[lists->i], lists->destleni);
+						/*
+						** put j into i
+						*/
+						lists->timearray[lists->i] = ft_strdup(lists->timearray[lists->j]);
+						lists->dest[lists->i] = ft_strdup(lists->dest[lists->j]);
+						/*
+						** clear our j
+						*/
+						ft_bzero(lists->timearray[lists->j], lists->timelenj);
+						ft_bzero(lists->dest[lists->j], lists->destlenj);
+						/*
+						** put temp into j
+						*/
+						lists->timearray[lists->j] = ft_strdup(lists->temp);
+						lists->dest[lists->j] = ft_strdup(lists->tempdest);
+						/*
+						** free the temps
+						*/
+						free(lists->temp);
+						free(lists->tempdest);
+					}
+				}
 				lists->j++;
 			}
 		}
-	// }
 }
