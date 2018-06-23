4026221915772c79d3c1e1c4a9eb1a7a72845154
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-06-23 17:20:08 -0400

    keep cleaning functions

--- ft_ls1/srcs/morehelpers.c
+++ ft_ls1/srcs/morehelpers.c
@@ -64,61 +64,6 @@ void	rt_helper_helper(DIR *dip, t_lists *lists, char *arg, int flag)
             read_helper_guts_a(lists, arg, dit, temp, 1);
     }
 }
-/*
-** -rl
-*/
-void    list_dirlr(int argc, char **argv, t_lists *lists)
-{
-    int             j;
-    DIR             *dip;
-    struct dirent   *dit;
-    struct stat     fileStat;
-    char            arg[WORD_MAX];
-    char            temp[WORD_MAX];
-
-    lists->size = 0;
-    lists->i = 0;
-    j = 2;
-    lists->flag = 0;
-    if (argc == 2)
-    {
-        argv[j] = ".";
-        argc = 3;
-    }
-    while (j < argc)
-    {
-        if(lstat(argv[j], &fileStat) < 0) 
-            ft_error("ls -l: No such file or directory");
-        if (((fileStat.st_mode & S_IFMT) == S_IFREG) || S_ISLNK(fileStat.st_mode))
-            ls_stat(argv[j], lists);
-        else if (S_ISDIR(fileStat.st_mode) == 1)
-        {
-        	/* -----------------------------------> */
-            dip = opendir(argv[j]);
-            ft_strcpy(arg, argv[j]);
-            if (ft_strcmp(arg, "/dev") == 0)
-                lists->flag = 1;
-            if (ft_strcmp(&arg[ft_strlen(arg) - 1], "/") != 0)
-                ft_strcat(arg, "/");
-            if (closedir(dip) == -1)
-                ft_error("closedir");
-            lists->filecount = directory_count(dip, argv[j], 0);
-            /* -----------------------------------> */
-            dip = opendir(argv[j]);
-            lists->dest = (char **)ft_memalloc(sizeof(char *) * lists->filecount  + 1);
-            if (dip == NULL)
-                ft_error(": No file or directory");
-            ft_strcpy(temp, arg);
-            while ((dit = readdir(dip)) != NULL)
-            	read_helper_guts(lists, arg, dit, temp, 0);
-            if (closedir(dip) == -1)
-                ft_error("closedir");
-        }
-        j++;
-    }
-    lex_sortrl(lists);
-    ls_stat_helper(lists);
-}
 
 /*
 ** -lt helper
