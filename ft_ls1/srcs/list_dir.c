b09ac8b989b16e2b597d45dae194b376b4a2b517
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-06-24 19:51:39 -0400

    ls l normed

--- ft_ls1/srcs/list_dir.c
+++ ft_ls1/srcs/list_dir.c
@@ -28,16 +28,42 @@ int		directory_count(DIR *dip, char *str, int flag)
 }
 
 /*
+** - ls -l helper
+*/
+static void ls_l_helper(struct stat fileStat, char *arg, char *str, t_lists *lists)
+{
+	DIR				*dip;
+	dip = NULL;
+
+	if (S_ISDIR(fileStat.st_mode) == 1)
+	{
+		dip = opendir(str);
+		ft_strcpy(arg, str);
+		if (ft_strcmp(arg, "/dev") == 0)
+			lists->flag = 1;
+		if (ft_strcmp(&arg[ft_strlen(arg) - 1], "/") != 0)
+			ft_strcat(arg, "/");
+		if (closedir(dip) == -1)
+			ft_error("closedir");
+		lists->filecount = directory_count(dip, str, 0);
+		dip = opendir(str);
+		read_helper(lists, 0, arg, dip);
+		if (closedir(dip) == -1)
+			ft_error("closedir");
+		ls_stat_helper(lists);
+	}
+
+}
+/*
 ** ls -l
 */
 void	list_dirl(int argc, char **argv, t_lists *lists)
 {
 	int				j;
-	DIR				*dip;
+	// DIR				*dip;
 	struct stat 	fileStat;
 	char			arg[WORD_MAX];
 
-/* ------------------------------------------> */
 	j = 1;
 	lists->flag = 0;
 	if (argc == 2)
@@ -45,31 +71,33 @@ void	list_dirl(int argc, char **argv, t_lists *lists)
 		argv[j + 1] = ".";
 		argc = 3;
 	}
-/* ------------------------------------------> */		
 	while (++j < argc)
 	{
+		if (j > 2 && j < argc)
+			write(1, "\n", 1);
 		lists->i = 0;
 		if(lstat(argv[j], &fileStat) < 0) 
         	ft_error("ls -l: No such file or directory");
 		if (((fileStat.st_mode & S_IFMT) == S_IFREG) || S_ISLNK(fileStat.st_mode))
 			ls_stat(argv[j], lists);
-		else if (S_ISDIR(fileStat.st_mode) == 1)
-		{
-			dip = opendir(argv[j]);
-			ft_strcpy(arg, argv[j]);
-			if (ft_strcmp(arg, "/dev") == 0)
-				lists->flag = 1;
-			if (ft_strcmp(&arg[ft_strlen(arg) - 1], "/") != 0)
-				ft_strcat(arg, "/");
-			if (closedir(dip) == -1)
-				ft_error("closedir");
-			lists->filecount = directory_count(dip, argv[j], 0);
-			dip = opendir(argv[j]);
-			read_helper(lists, 0, arg, dip);
-			if (closedir(dip) == -1)
-				ft_error("closedir");
-			ls_stat_helper(lists);
-		}
+		ls_l_helper(fileStat, arg, argv[j], lists);
+		// else if (S_ISDIR(fileStat.st_mode) == 1)
+		// {
+		// 	dip = opendir(argv[j]);
+		// 	ft_strcpy(arg, argv[j]);
+		// 	if (ft_strcmp(arg, "/dev") == 0)
+		// 		lists->flag = 1;
+		// 	if (ft_strcmp(&arg[ft_strlen(arg) - 1], "/") != 0)
+		// 		ft_strcat(arg, "/");
+		// 	if (closedir(dip) == -1)
+		// 		ft_error("closedir");
+		// 	lists->filecount = directory_count(dip, argv[j], 0);
+		// 	dip = opendir(argv[j]);
+		// 	read_helper(lists, 0, arg, dip);
+		// 	if (closedir(dip) == -1)
+		// 		ft_error("closedir");
+		// 	ls_stat_helper(lists);
+		// }
 	}
 }
 /*
