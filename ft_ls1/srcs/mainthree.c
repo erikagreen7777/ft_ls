470fa812606aa308c283c354c9b974fea629c68d
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-06-25 10:36:04 -0400

    Rlt

--- ft_ls1/srcs/mainthree.c
+++ ft_ls1/srcs/mainthree.c
@@ -114,14 +114,16 @@ void	main_ten(/*int argc, */char **argv, t_lists *lists)
 		lists->big_r_flag > 0 && lists->lflag > 0 && \
 		lists->aflag == 0)
 	{
-		// if (argc == 2)
-		// {
-		//     argv[2] = ".";
-		//     argc = 3;
-		// }
+		int j;
+		j = lists->argcount - 1;
 		lists->i = 0;
-		rlt_helper(argv[lists->argcount], 0, lists);
-		rlt(argv[lists->argcount], 0, lists);
+		while (++j < lists->newargc)
+		{		
+			if (j > lists->argcount && j < lists->newargc)
+				write(1, "\n", 1);
+			rlt_helper(argv[j], 0, lists);
+			rlt(argv[j], 0, lists);
+		}
 		exit (0);
 	}
 	/*
