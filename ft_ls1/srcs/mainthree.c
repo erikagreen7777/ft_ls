98c139e8729fdf57411f2f0420975cbd917795d9
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-06-25 10:37:41 -0400

    Rla

--- ft_ls1/srcs/mainthree.c
+++ ft_ls1/srcs/mainthree.c
@@ -9,14 +9,16 @@ void	main_seven(/*int argc, */char **argv, t_lists *lists)
 		lists->big_r_flag > 0 && lists->lflag > 0 && \
 		lists->aflag > 0)			
 	{
-		// if (argc == 2)
-		// {
-		//     argv[2] = ".";
-		//     argc = 3;
-		// }
+		int j;
+		j = lists->argcount - 1;
 		lists->i = 0;
-		Rl_helper(argv[lists->argcount], 1, lists);
-		list_dirbigrl(argv[lists->argcount], 1, lists);
+		while (++j < lists->newargc)
+		{		
+			if (j > lists->argcount && j < lists->newargc)
+				write(1, "\n", 1);
+			Rl_helper(argv[j], 1, lists);
+			list_dirbigrl(argv[j], 1, lists);
+		}
 		exit (0);
 	}
 	/*
