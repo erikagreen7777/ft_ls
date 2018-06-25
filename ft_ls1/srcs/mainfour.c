313ac64b9ebda061395505a437a2fb8dacff82e2
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-06-25 11:03:38 -0400

    rRta

--- ft_ls1/srcs/mainfour.c
+++ ft_ls1/srcs/mainfour.c
@@ -51,14 +51,16 @@ void	main_thirteen(int argc, char **argv, t_lists *lists)
 		lists->big_r_flag > 0 && lists->lflag == 0 && \
 		lists->aflag > 0)			
 	{
-		if (argc == 2)
-		{
-		    argv[2] = ".";
-		    argc = 3;
-		}
+		int j;
+		j = lists->argcount - 1;
 		lists->i = 0;
-		rbigrt_helper(argv[2], 1, lists);
-		rbigrt(argv[2], 1, lists);
+		while (++j < lists->newargc)
+		{		
+			if (j > lists->argcount && j < lists->newargc)
+				write(1, "\n", 1);
+			rbigrt_helper(argv[j], 1, lists);
+			rbigrt(argv[j], 1, lists);
+		}
 		exit(0);
 	}
 	/*
