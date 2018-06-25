03fcc17b1bf7e0daa0ead65a34b9345873cd7215
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-06-25 18:48:34 -0400

    Rl no leaks at 2 args

--- ft_ls1/srcs/maintwo.c
+++ ft_ls1/srcs/maintwo.c
@@ -221,14 +221,17 @@ void	main_six(/*int argc, */char **argv, t_lists *lists)
 		lists->big_r_flag > 0 && lists->lflag > 0 && \
 		lists->aflag == 0)			
 	{
-		// if (argc == 2)
-		// {
-		//     argv[2] = ".";
-		//     argc = 3;
-		// }
+		int j;
+		j = lists->argcount - 1;
 		lists->i = 0;
-		Rl_helper(argv[lists->argcount], 0, lists);
-		list_dirbigrl(argv[lists->argcount], 0, lists);
+		while (++j < lists->newargc)
+		{		
+			if (j > lists->argcount && j < lists->newargc)
+				write(1, "\n", 1);
+			lists->i = 0;
+			Rl_helper(argv[lists->argcount], 0, lists);
+			list_dirbigrl(argv[lists->argcount], 0, lists);
+		}
 		exit (0);
 	}
 }
