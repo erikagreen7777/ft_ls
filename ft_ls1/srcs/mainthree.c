8fa541a3b7aac7e3641e2a393f6a279623999c2f
Katherine Green <kategreen03@Katherine-Greens-MacBook-Pro-5.local>
2018-06-25 10:10:36 -0400

    rRt done. made it like rRl

--- ft_ls1/srcs/mainthree.c
+++ ft_ls1/srcs/mainthree.c
@@ -60,14 +60,16 @@ void	main_eight(int argc, char **argv, t_lists *lists)
 		lists->big_r_flag > 0 && lists->lflag == 0 && \
 		lists->aflag == 0)				
 	{
-		// if (argc == 2)
-		// {
-		//     argv[2] = ".";
-		//     argc = 3;
-		// }
+		int j;
+		j = lists->argcount - 1;
 		lists->i = 0;
-		rbigrt_helper(argv[lists->argcount], 0, lists);
-		rbigrt(argv[lists->argcount], 0, lists);
+		while (++j < lists->newargc)
+		{		
+			if (j > lists->argcount && j < lists->newargc)
+				write(1, "\n", 1);
+			rbigrt_helper(argv[j], 0, lists);
+			rbigrt(argv[j], 0, lists);
+		}
 		exit (0);
 	}
 }
@@ -129,11 +131,7 @@ void	main_ten(/*int argc, */char **argv, t_lists *lists)
 		lists->big_r_flag > 0 && lists->lflag == 0 && \
 		lists->aflag > 0)
 	{
-		// if (argc == 2)
-		// {
-		//     argv[2] = ".";
-		//     argc = 3;
-		// }
+
 		lists->i = 0;
 		Rr_helper(argv[lists->argcount], 1, lists);
 		list_dirbigrr(argv[lists->argcount], 1, lists);
